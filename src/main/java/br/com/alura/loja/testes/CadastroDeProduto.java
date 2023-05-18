549e57438014729c83dfc0e17dc94896f50928e7
celina <celinaoliveira98@gmail.com>
2023-05-18 20:48:26 -0300

    mapeamento de relacionamento

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,27 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto("Xiaomi Readmi", "Muito bacana", new BigDecimal("800"), Categoria.CELULARES);
+
+        EntityManager em = JPAUtil.entityManager();
+        ProdutoDao dao = new ProdutoDao(em);
+
+        em.getTransaction().begin();
+        dao.cadastrar(celular);
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
